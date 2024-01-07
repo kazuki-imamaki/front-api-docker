cfcb15d28fbc97d763a72e73b6b21b16dcba5609
kazuki-imamaki <elm.fb.gradual@gmail.com>
2024-01-07 11:21:43 +0900

    first commit offront and api

---
+++ api/src/main/java/com/example/frontapidocker/FrontApiDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontapidocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontApiDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontApiDockerApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/frontapidocker/FrontApiDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontapidocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApiDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
